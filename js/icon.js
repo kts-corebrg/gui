85ac8b4aeeeb2129089f79744e89b9e265d14f24
corebrg2019 <tomato322@hanmail.net>
2019-12-19 15:44:52 +0900

    이미지 상대경로 수정

--- js/icon.js
+++ js/icon.js
@@ -3,8 +3,8 @@ var ITAhM = ITAhM || {};
 ITAhM.iconData = {
     "server": {
         "group": "server",
-        "src": "img/svg/server.svg",
-        "disabled": "img/svg/disabled/server.svg",
+        "src": "/img/svg/server.svg",
+        "disabled": "/img/svg/disabled/server.svg",
         unit: 1,
         color: "",
         texture: "/img/3d/server-1u.png",
@@ -13,8 +13,8 @@ ITAhM.iconData = {
     "mainframe": {
         "group": "server",
 
-        "src": "img/svg/mainframe.svg",
-        "disabled": "img/svg/disabled/mainframe.svg",
+        "src": "/img/svg/mainframe.svg",
+        "disabled": "/img/svg/disabled/mainframe.svg",
         unit: 1,
         color: "",
         texture: "/img/3d/server-1u.png",
@@ -22,16 +22,16 @@ ITAhM.iconData = {
     },
     "database": {
         "group": "server",
-        "src": "img/svg/database.svg",
-        "disabled": "img/svg/disabled/database.svg",
+        "src": "/img/svg/database.svg",
+        "disabled": "/img/svg/disabled/database.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "storage": {
         "group": "server",
-        "src": "img/svg/storage.svg",
-        "disabled": "img/svg/disabled/storage.svg",
+        "src": "/img/svg/storage.svg",
+        "disabled": "/img/svg/disabled/storage.svg",
         unit: 2,
         color: "",
         texture: "/img/3d/storage.png",
@@ -39,24 +39,24 @@ ITAhM.iconData = {
     },
     "network": {
         "group": "network",
-        "src": "img/svg/network.svg",
-        "disabled": "img/svg/disabled/network.svg",
+        "src": "/img/svg/network.svg",
+        "disabled": "/img/svg/disabled/network.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "nms": {
         "group": "network",
-        "src": "img/svg/nms.svg",
-        "disabled": "img/svg/disabled/nms.svg",
+        "src": "/img/svg/nms.svg",
+        "disabled": "/img/svg/disabled/nms.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "workgroup switch": {
         "group": "network",
-        "src": "img/svg/workgroup.svg",
-        "disabled": "img/svg/disabled/workgroup.svg",
+        "src": "/img/svg/workgroup.svg",
+        "disabled": "/img/svg/disabled/workgroup.svg",
         "texture": "/img/3d/wg.png",
         top: "/img/3d/wg-top.png",
         color: "",
@@ -64,8 +64,8 @@ ITAhM.iconData = {
     },
     "layer4 switch": {
         "group": "network",
-        "src": "img/svg/l4switch.svg",
-        "disabled": "img/svg/disabled/l4switch.svg",
+        "src": "/img/svg/l4switch.svg",
+        "disabled": "/img/svg/disabled/l4switch.svg",
         unit: 6,
         color: "",
         texture: "/img/3d/l4.png",
@@ -73,8 +73,8 @@ ITAhM.iconData = {
     },
     "layer3 switch": {
         "group": "network",
-        "src": "img/svg/l3switch.svg",
-        "disabled": "img/svg/disabled/l3switch.svg",
+        "src": "/img/svg/l3switch.svg",
+        "disabled": "/img/svg/disabled/l3switch.svg",
         unit: 6,
         color: "",
         texture: "/img/3d/l3.png",
@@ -82,8 +82,8 @@ ITAhM.iconData = {
     },
     "router": {
         "group": "network",
-        "src": "img/svg/router.svg",
-        "disabled": "img/svg/disabled/router.svg",
+        "src": "/img/svg/router.svg",
+        "disabled": "/img/svg/disabled/router.svg",
         unit: 2,
         color: "",
         texture: "/img/3d/router.png",
@@ -91,8 +91,8 @@ ITAhM.iconData = {
     },
     "firewall": {
         "group": "network",
-        "src": "img/svg/firewall.svg",
-        "disabled": "img/svg/disabled/firewall.svg",
+        "src": "/img/svg/firewall.svg",
+        "disabled": "/img/svg/disabled/firewall.svg",
         texture: "/img/3d/firewall.png",
         top: "/img/3d/firewall-top.png",
         color: "",
@@ -100,16 +100,16 @@ ITAhM.iconData = {
     },
     "vpn gateway": {
         "group": "network",
-        "src": "img/svg/vpn.svg",
-        "disabled": "img/svg/disabled/vpn.svg",
+        "src": "/img/svg/vpn.svg",
+        "disabled": "/img/svg/disabled/vpn.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "vpn concentrator": {
         "group": "network",
-        "src": "img/svg/concentrator.svg",
-        "disabled": "img/svg/disabled/concentrator.svg",
+        "src": "/img/svg/concentrator.svg",
+        "disabled": "/img/svg/disabled/concentrator.svg",
         unit: 2,
         color: "",
         texture: "/img/3d/concentrator.png",
@@ -117,16 +117,16 @@ ITAhM.iconData = {
     },
     "wireless": {
         "group": "network",
-        "src": "img/svg/wireless.svg",
-        "disabled": "img/svg/disabled/wireless.svg",
+        "src": "/img/svg/wireless.svg",
+        "disabled": "/img/svg/disabled/wireless.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "accesspoint": {
         "group": "network",
-        "src": "img/svg/accesspoint.svg",
-        "disabled": "img/svg/disabled/accesspoint.svg",
+        "src": "/img/svg/accesspoint.svg",
+        "disabled": "/img/svg/disabled/accesspoint.svg",
         unit: 1,
         color: "",
         texture: "/img/3d/wifi.png",
@@ -134,8 +134,8 @@ ITAhM.iconData = {
     },
     "accelerator": {
         "group": "network",
-        "src": "img/svg/accelerator.svg",
-        "disabled": "img/svg/disabled/accelerator.svg",
+        "src": "/img/svg/accelerator.svg",
+        "disabled": "/img/svg/disabled/accelerator.svg",
         unit: 4,
         color: "",
         texture: "/img/3d/accelerator.png",
@@ -143,16 +143,16 @@ ITAhM.iconData = {
     },
     "nac": {
         "group": "network",
-        "src": "img/svg/nac.svg",
-        "disabled": "img/svg/disabled/nac.svg",
+        "src": "/img/svg/nac.svg",
+        "disabled": "/img/svg/disabled/nac.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "printer": {
     	"group": "etc",
-    	"src": "img/svg/printer.svg",
-        "disabled": "img/svg/disabled/printer.svg",
+    	"src": "/img/svg/printer.svg",
+        "disabled": "/img/svg/disabled/printer.svg",
         unit: 2,
         color: "",
         texture: "/img/3d/printer.png",
@@ -160,16 +160,16 @@ ITAhM.iconData = {
     },
     "office": {
         "group": "group",
-        "src": "img/svg/office.svg",
-        "disabled": "img/svg/disabled/office.svg",
+        "src": "/img/svg/office.svg",
+        "disabled": "/img/svg/disabled/office.svg",
         texture: "/img/3d/office.png",
         color: 0x333333,
         unit: 8
     },
     "cloud": {
         "group": "etc",
-        "src": "img/svg/cloud.svg",
-        "disabled": "img/svg/disabled/cloud.svg",
+        "src": "/img/svg/cloud.svg",
+        "disabled": "/img/svg/disabled/cloud.svg",
         unit: 2,
         color: "",
         texture: "/img/3d/cloud.png",
@@ -177,56 +177,56 @@ ITAhM.iconData = {
     },
     "unknown": {
         "group": "etc",
-        "src": "img/svg/unknown.svg",
-        "disabled": "img/svg/disabled/unknown.svg",
+        "src": "/img/svg/unknown.svg",
+        "disabled": "/img/svg/disabled/unknown.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "phone": {
         "group": "etc",
-        "src": "img/svg/phone.svg",
-        "disabled": "img/svg/disabled/phone.svg",
+        "src": "/img/svg/phone.svg",
+        "disabled": "/img/svg/disabled/phone.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "laptop": {
         "group": "etc",
-        "src": "img/svg/laptop.svg",
-        "disabled": "img/svg/disabled/laptop.svg",
+        "src": "/img/svg/laptop.svg",
+        "disabled": "/img/svg/disabled/laptop.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "cctv": {
         "group": "etc",
-        "src": "img/svg/camera.svg",
-        "disabled": "img/svg/disabled/camera.svg",
+        "src": "/img/svg/camera.svg",
+        "disabled": "/img/svg/disabled/camera.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "ups": {
         "group": "etc",
-        "src": "img/svg/ups.svg",
-        "disabled": "img/svg/disabled/ups.svg",
+        "src": "/img/svg/ups.svg",
+        "disabled": "/img/svg/disabled/ups.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "video recorder": {
     	"group": "etc",
-        "src": "img/svg/videorecorder.svg",
-        "disabled": "img/svg/disabled/videorecorder.svg",
+        "src": "/img/svg/videorecorder.svg",
+        "disabled": "/img/svg/disabled/videorecorder.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "security": {
     	"group": "etc",
-        "src": "img/svg/security.svg",
-        "disabled": "img/svg/disabled/security.svg",
+        "src": "/img/svg/security.svg",
+        "disabled": "/img/svg/disabled/security.svg",
         unit: 2,
         color: "",
         texture: "/img/3d/security.png",
@@ -234,16 +234,16 @@ ITAhM.iconData = {
     },
     "group": {
         "group": "group",
-        "src": "img/svg/group.svg",
-        "disabled": "img/svg/disabled/group.svg",
+        "src": "/img/svg/group.svg",
+        "disabled": "/img/svg/disabled/group.svg",
         unit: 1,
         color: "0xdddddd",
         texture: ""
     },
     "application": {
         "group": "application",
-        "src": "img/svg/application.svg",
-        "disabled": "img/svg/disabled/application.svg",
+        "src": "/img/svg/application.svg",
+        "disabled": "/img/svg/disabled/application.svg",
         unit: 1,
         color: "",
         texture: "/img/3d/app.png",
@@ -251,112 +251,112 @@ ITAhM.iconData = {
     },
     "AP": {
         "group": "sos",
-        "src": "img/kts/normal/ap.png",
-        "disabled": "img/kts/normal/ap.png",
-        "shutdown": "img/kts/shutdown/ap.png"
+        "src": "/img/kts/normal/ap.png",
+        "disabled": "/img/kts/normal/ap.png",
+        "shutdown": "/img/kts/shutdown/ap.png"
     },
     "카메라": {
         "group": "sos",
-        "src": "img/kts/normal/camera.png",
-        "disabled": "img/kts/normal/camera.png",
-        "shutdown": "img/kts/shutdown/camera.png"
+        "src": "/img/kts/normal/camera.png",
+        "disabled": "/img/kts/normal/camera.png",
+        "shutdown": "/img/kts/shutdown/camera.png"
     },
     "DMZ": {
         "group": "sos",
-        "src": "img/kts/normal/dmz.png",
-        "disabled": "img/kts/normal/dmz.png",
-        "shutdown": "img/kts/shutdown/dmz.png"
+        "src": "/img/kts/normal/dmz.png",
+        "disabled": "/img/kts/normal/dmz.png",
+        "shutdown": "/img/kts/shutdown/dmz.png"
     },
     "출입문": {
         "group": "sos",
-        "src": "img/kts/normal/door.png",
-        "disabled": "img/kts/normal/door.png",
-        "shutdown": "img/kts/shutdown/door.png"
+        "src": "/img/kts/normal/door.png",
+        "disabled": "/img/kts/normal/door.png",
+        "shutdown": "/img/kts/shutdown/door.png"
     },
     "ERP": {
         "group": "sos",
-        "src": "img/kts/normal/erp.png",
-        "disabled": "img/kts/normal/erp.png",
-        "shutdown": "img/kts/shutdown/erp.png"
+        "src": "/img/kts/normal/erp.png",
+        "disabled": "/img/kts/normal/erp.png",
+        "shutdown": "/img/kts/shutdown/erp.png"
     },
     "허브": {
         "group": "sos",
-        "src": "img/kts/normal/hub.png",
-        "disabled": "img/kts/normal/hub.png",
-        "shutdown": "img/kts/shutdown/hub.png"
+        "src": "/img/kts/normal/hub.png",
+        "disabled": "/img/kts/normal/hub.png",
+        "shutdown": "/img/kts/shutdown/hub.png"
     },
     "L2스위치": {
         "group": "sos",
-        "src": "img/kts/normal/l2.png",
-        "disabled": "img/kts/normal/l2.png",
-        "shutdown": "img/kts/shutdown/l2.png"
+        "src": "/img/kts/normal/l2.png",
+        "disabled": "/img/kts/normal/l2.png",
+        "shutdown": "/img/kts/shutdown/l2.png"
     },
     "L3스위치": {
         "group": "sos",
-        "src": "img/kts/normal/l3.png",
-        "disabled": "img/kts/normal/l3.png",
-        "shutdown": "img/kts/shutdown/l3.png"
+        "src": "/img/kts/normal/l3.png",
+        "disabled": "/img/kts/normal/l3.png",
+        "shutdown": "/img/kts/shutdown/l3.png"
     },
     "NAS": {
         "group": "sos",
-        "src": "img/kts/normal/nas.png",
-        "disabled": "img/kts/normal/nas.png",
-        "shutdown": "img/kts/shutdown/nas.png"
+        "src": "/img/kts/normal/nas.png",
+        "disabled": "/img/kts/normal/nas.png",
+        "shutdown": "/img/kts/shutdown/nas.png"
     },
     "노트북": {
         "group": "sos",
-        "src": "img/kts/normal/notebook.png",
-        "disabled": "img/kts/normal/notebook.png",
-        "shutdown": "img/kts/shutdown/notebook.png"
+        "src": "/img/kts/normal/notebook.png",
+        "disabled": "/img/kts/normal/notebook.png",
+        "shutdown": "/img/kts/shutdown/notebook.png"
     },
     
     "컴퓨터": {
         "group": "sos",
-        "src": "img/kts/normal/pc.png",
-        "disabled": "img/kts/normal/pc.png",
-        "shutdown": "img/kts/shutdown/pc.png"
+        "src": "/img/kts/normal/pc.png",
+        "disabled": "/img/kts/normal/pc.png",
+        "shutdown": "/img/kts/shutdown/pc.png"
     },
     "전화기": {
         "group": "sos",
-        "src": "img/kts/normal/phone.png",
-        "disabled": "img/kts/normal/phone.png",
-        "shutdown": "img/kts/shutdown/phone.png"
+        "src": "/img/kts/normal/phone.png",
+        "disabled": "/img/kts/normal/phone.png",
+        "shutdown": "/img/kts/shutdown/phone.png"
     },
     "프린터": {
         "group": "sos",
-        "src": "img/kts/normal/printer.png",
-        "disabled": "img/kts/normal/printer.png",
-        "shutdown": "img/kts/shutdown/printer.png"
+        "src": "/img/kts/normal/printer.png",
+        "disabled": "/img/kts/normal/printer.png",
+        "shutdown": "/img/kts/shutdown/printer.png"
     },
     "라벨프린터": {
         "group": "sos",
-        "src": "img/kts/normal/printer_label.png",
-        "disabled": "img/kts/normal/printer_label.png",
-        "shutdown": "img/kts/shutdown/printer_label.png"
+        "src": "/img/kts/normal/printer_label.png",
+        "disabled": "/img/kts/normal/printer_label.png",
+        "shutdown": "/img/kts/shutdown/printer_label.png"
     },
     "복합기": {
         "group": "sos",
-        "src": "img/kts/normal/printer_multi.png",
-        "disabled": "img/kts/normal/printer_multi.png",
-        "shutdown": "img/kts/shutdown/printer_multi.png"
+        "src": "/img/kts/normal/printer_multi.png",
+        "disabled": "/img/kts/normal/printer_multi.png",
+        "shutdown": "/img/kts/shutdown/printer_multi.png"
     },
     "서버": {
         "group": "sos",
-        "src": "img/kts/normal/server.png",
-        "disabled": "img/kts/normal/server.png",
-        "shutdown": "img/kts/shutdown/server.png"
+        "src": "/img/kts/normal/server.png",
+        "disabled": "/img/kts/normal/server.png",
+        "shutdown": "/img/kts/shutdown/server.png"
     },
     "공유기": {
         "group": "sos",
-        "src": "img/kts/normal/share.png",
-        "disabled": "img/kts/normal/share.png",
-        "shutdown": "img/kts/shutdown/share.png"
+        "src": "/img/kts/normal/share.png",
+        "disabled": "/img/kts/normal/share.png",
+        "shutdown": "/img/kts/shutdown/share.png"
     },
     "VPN": {
         "group": "sos",
-        "src": "img/kts/normal/vpn.png",
-        "disabled": "img/kts/normal/vpn.png",
-        "shutdown": "img/kts/shutdown/vpn.png"
+        "src": "/img/kts/normal/vpn.png",
+        "disabled": "/img/kts/normal/vpn.png",
+        "shutdown": "/img/kts/shutdown/vpn.png"
     }
 };
 
